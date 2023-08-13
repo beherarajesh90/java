2c8930d7a728fb4bee3379a703458ab754352874
Rajesh <beherarajesh90@gmail.com>
2023-08-13 23:19:18 +0530

    Heap implementation in java

---
+++ src/com/java/dsa/heap/HeapInJava.java
@@ -0,0 +1,28 @@
+package com.java.dsa.heap;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class HeapInJava {
+    public static void main(String[] args) {
+        //MAX HEAP
+        Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        //MIN HEAP
+//        Queue<Integer> minHeap = new PriorityQueue<>();
+
+        maxHeap.add(10);
+        maxHeap.add(30);
+        maxHeap.add(20);
+        maxHeap.add(5);
+        maxHeap.add(1);
+
+        while (!maxHeap.isEmpty()){
+            System.out.print(maxHeap.peek()+" ");
+            //remove
+            maxHeap.poll();
+        }
+    }
+}
