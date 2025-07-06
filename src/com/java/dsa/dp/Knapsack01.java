2f3ad372ff83786be0d34e315174d37eb9bccc03
Rajesh <beherarajesh90@gmail.com>
2025-07-06 16:48:17 +0530

    https://www.geeksforgeeks.org/batch/dsa-4/track/DSASP-DP/problem/0-1-knapsack-problem0945

---
+++ src/com/java/dsa/dp/Knapsack01.java
@@ -0,0 +1,38 @@
+package com.java.dsa.dp;
+
+class Knapsack01 {
+    static int knapsack(int W, int val[], int wt[]) {
+        // code here
+        if(W <=0 || val.length==0 || val.length!=wt.length)
+            return 0;
+
+        int n = val.length;
+        int[] dp = new int[W+1];
+
+        for(int c=0; c<=W; c++){
+            if(wt[0]<=c){
+                dp[c]=val[0];
+            }
+        }
+
+        for(int i=1;i<n;i++){
+            for(int c=W; c>=0; c--){
+                int included=0,excluded=0;
+                if(wt[i]<=c){
+                    included = val[i]+dp[c-wt[i]];
+                }
+                excluded = dp[c];
+                dp[c] = Math.max(included, excluded);
+            }
+        }
+        return dp[W];
+    }
+
+    public static void main(String[] args) {
+        int val[] = {1,2,3};
+        int wt[] = {4, 5, 1};
+        int W = 4;
+        System.out.println(knapsack(W, val, wt));
+    }
+}
+
