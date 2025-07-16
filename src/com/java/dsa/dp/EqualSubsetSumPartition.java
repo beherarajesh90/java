d1500064ea26979acf1d28983802a4c0b47f86f3
Rajesh <beherarajesh90@gmail.com>
2025-07-16 22:52:10 +0530

    https://leetcode.com/problems/partition-equal-subset-sum/description/?envType=problem-list-v2&envId=dynamic-programming

---
+++ src/com/java/dsa/dp/EqualSubsetSumPartition.java
@@ -0,0 +1,25 @@
+package com.java.dsa.dp;
+
+public class EqualSubsetSumPartition {
+    public static boolean canPartition(int[] nums, int sum) {
+        int n = nums.length;
+        boolean[] dp = new boolean[sum+1];
+        dp[0] = true;
+        for(int s=1; s<=sum; s++){
+            dp[s] = nums[0] == s;
+        }
+
+        for(int i=1;i<n; i++){
+            for(int s=sum; s>=0; s--){
+                if(!dp[s] && s>=nums[i]) dp[s] = dp[s-nums[i]];
+            }
+        }
+
+        return dp[sum];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 5, 11, 5};
+        System.out.println(canPartition(nums, 12));
+    }
+}
---
+++ src/com/java/dsa/dp/EqualSumPartition.java
@@ -0,0 +1,29 @@
+package com.java.dsa.dp;
+
+public class EqualSumPartition {
+    public boolean canPartition(int[] nums) {
+        int total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+        if (total % 2 == 1)
+            return false;
+        int sum = total / 2;
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int s = sum; s >= num; s--) {
+                dp[s] = dp[s] || dp[s - num];
+            }
+        }
+
+        return dp[sum];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 5, 11, 5};
+        System.out.println(new EqualSumPartition().canPartition(nums));
+    }
+
+}
